90297754b61f65c4ef8819662029ddf346b745c9
ayush tyagi <ayushtyagi4810poco@gmail.com>
2024-01-18 20:57:47 +0530

    company card

--- src/constants/CompanyCard.jsx
+++ src/constants/CompanyCard.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const CompanyCard = () => {
     return (
-        <div>CompanyCard</div>
+        <div>
+            Here comes a carousel self made
+        </div>
     )
 }
 
