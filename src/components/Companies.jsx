4f837c9a11a632b5f7017bde8889172b2e45acab
ayush tyagi <ayushtyagi4810poco@gmail.com>
2024-01-23 12:44:29 +0530

    completed the tech news page

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,11 +1,93 @@
+// import React, { useEffect, useRef, useState } from 'react';
+// import Header from './Header';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faSearch } from '@fortawesome/free-solid-svg-icons';
+// import NewsComponent from './NewsComponent';
+// const Companies = () => {
+//     const [searchTerm, setSearchTerm] = useState('html');
+//     const inputRef = useRef();
+//     const [newObject, setNewsObject] = useState([]);
+//     const url = `http://hn.algolia.com/api/v1/search?query=${searchTerm}`;
+
+//     const handleSearchNews = async () => {
+//         try {
+//             const response = await fetch(url);
+
+//             if (!response.ok) {
+//                 throw new Error(`Failed to fetch data. Status: ${response.status}`);
+//             }
+//             const data = await response.json();
+//             setNewsObject(data.hits);
+
+//         } catch (error) {
+//             console.error('Error fetching data:', error.message);
+//         }
+//     };
+//     const handleInputChange = () => {
+//         setSearchTerm(inputRef.current.value);
+//     };
+
+//     useEffect(() => {
+//         handleSearchButtonClick();
+//     }, [])
+
+//     const handleSearchButtonClick = () => {
+//         handleSearchNews();
+//     };
+
+//     return (
+//         <div>
+//             <Header />
+//             <div className='flex flex-col items-center'>
+//                 <h1 className='text-4xl p-2 font-bold' >
+//                     Tech <span className='text-purple-700'>News</span> on the <span className='text-purple-700'>Go</span>
+//                 </h1>
+//             </div>
+//             <div className='flex justify-center'>
+//                 <div>
+//                     {/* here comes the Search Bar */}
+//                     <input
+//                         ref={inputRef}
+//                         className='ml-8 text-xl w-96 rounded-2xl shadow-xl m-4 border-2 border-black p-4'
+//                         type="text"
+//                         placeholder='search about any technology....'
+//                         onChange={handleInputChange}
+//                     />
+//                     <button
+//                         className=' relative left-[-55px] top-[2px]'
+//                         onClick={handleSearchButtonClick}
+//                     >
+//                         <FontAwesomeIcon className='text-xl' icon={faSearch} />
+//                     </button>
+//                 </div>
+//             </div>
+//             <div className='flex flex-col items-center'>
+
+//                 {
+//                     newObject.map((news, id) => (
+//                         <NewsComponent id={id} title={news.title} author={news.author} readMore={news.url} comments={news.num_comments} />
+//                     ))
+//                 }
+
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Companies;
+
 import React, { useEffect, useRef, useState } from 'react';
 import Header from './Header';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import NewsComponent from './NewsComponent';
+import ShimmerUi from "../constants/ShimmerUi"
+
 const Companies = () => {
     const [searchTerm, setSearchTerm] = useState('html');
     const inputRef = useRef();
+    const [newObject, setNewsObject] = useState([]);
+    const [loading, setLoading] = useState(true); // New loading state
     const url = `http://hn.algolia.com/api/v1/search?query=${searchTerm}`;
 
     const handleSearchNews = async () => {
@@ -16,9 +98,12 @@ const Companies = () => {
                 throw new Error(`Failed to fetch data. Status: ${response.status}`);
             }
             const data = await response.json();
-            console.log(data);
+            setNewsObject(data.hits);
+
         } catch (error) {
             console.error('Error fetching data:', error.message);
+        } finally {
+            setLoading(false); // Set loading to false whether fetching was successful or not
         }
     };
 
@@ -26,7 +111,12 @@ const Companies = () => {
         setSearchTerm(inputRef.current.value);
     };
 
+    useEffect(() => {
+        handleSearchButtonClick();
+    }, []);
+
     const handleSearchButtonClick = () => {
+        setLoading(true); // Set loading to true when starting to fetch data
         handleSearchNews();
     };
 
@@ -49,7 +139,7 @@ const Companies = () => {
                         onChange={handleInputChange}
                     />
                     <button
-                        className=' relative left-[-55px] top-[2px] '
+                        className=' relative left-[-55px] top-[2px]'
                         onClick={handleSearchButtonClick}
                     >
                         <FontAwesomeIcon className='text-xl' icon={faSearch} />
@@ -57,7 +147,15 @@ const Companies = () => {
                 </div>
             </div>
             <div className='flex flex-col items-center'>
-                <NewsComponent />
+
+                {loading ? (
+                    <p className='mt-8 text-4xl text-gray-800 font-serif font-semibold '>Loading...</p>
+                ) : (
+                    newObject.map((news, id) => (
+                        <NewsComponent key={id} title={news.title} author={news.author} readMore={news.url} comments={news.num_comments} />
+                    ))
+                )}
+
             </div>
         </div>
     );
--- src/components/NewsComponent.jsx
+++ src/components/NewsComponent.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const NewsComponent = () => {
+const NewsComponent = ({ title, author, readMore, comments }) => {
     return (
-        <div className='text-3xl flex flex-wrap items-center w-[50%]'>
-            Up until now I am getting the api result and seeing it in the console now I need to update the news Component card to add the details and then show it on the page
+        <div className='bg-purple-200 rounded-xl shadow-xl  p-5 m-8  flex flex-col flex-wrap w-[50%]'>
+            <h1 className='text-3xl text-gray-900 font-serif font-semibold '>
+                {title}
+            </h1>
+            <p className='text-lg font-medium text-gray-700 pt-2'>
+                By- {author} | {comments} comments
+            </p>
+            <Link to={readMore} target='_blank' >
+                <p className='pt-2 text-red-700 font-medium text-xl'>
+                    Read More
+                </p>
+            </Link>
         </div>
     )
 }
