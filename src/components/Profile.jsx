398031a95c3733c1077270b085a9706a651fca5d
Ayush Tyagi <86391985+IayushCoderJOD@users.noreply.github.com>
2024-05-28 16:50:15 +0530

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -29,11 +29,11 @@ const Profile = () => {
                     <div>
                         <img className='opacity-80 h-52 w-full rounded-lg ' src="https://img.freepik.com/free-vector/abstract-banner-with-low-poly-plexus-network-communications-design_1048-12914.jpg?w=1380&t=st=1705741639~exp=1705742239~hmac=b9ad16ab2d9f3e7645b089f6a70ffb1e2b310011765e7110c1ec71ac549168de" alt="" />
                     </div>
-                    <img src={profilePic} className='md:w-48 md:h-48 absolute right-[22%] top-[30%] h-32 w-32 border-4 border-white  rounded-full shadow-2xl ' alt="" />
+                    <img src="https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png" className='md:w-48 md:h-48 absolute right-[22%] top-[30%] h-32 w-32 border-4 border-white  rounded-full shadow-2xl ' alt="" />
                     {/* below div will be the information page */}
                     <div className='flex justify-start'>
                         <div>
-                            <h1 className='font-medium text-2xl pl-5 pt-2 font-serif '>Ayush Tyagi <span className="text-gray-600 text-xl font-light" >(He/Him)</span></h1>
+                            <h1 className='font-medium text-2xl pl-5 pt-2 font-serif '>Ishita Goswami <span className="text-gray-600 text-xl font-light" >(She/Her)</span></h1>
                         </div>
                         <div className='flex pl-[215px]'>
                             <img className='h-8 m-1' src="https://media.licdn.com/dms/image/C510BAQHFNX62-HXwqA/company-logo_100_100/0/1631428673516/kiet_group_of_institutions_logo?e=1714003200&v=beta&t=uFdjfrJ14Av1MuHhjN6bYstgZXv5wercvoYWs26_GHE" alt="" />
@@ -49,8 +49,8 @@ const Profile = () => {
                             <li className='mb-2'> <FontAwesomeIcon className='pr-1 ' icon={faCalendar} /> Add availability to join</li>
                         </ul>
                         <ul className='ml-72 mt-2 text-md text-gray-700'>
-                            <li> <FontAwesomeIcon className='pr-1 ' icon={faPhone} />  9520613623</li>
-                            <li><FontAwesomeIcon className='pr-1 ' icon={faMessage} /> ayushtyagi4810poco@gmail.com</li>
+                            <li> <FontAwesomeIcon className='pr-1 ' icon={faPhone} />  7417897075</li>
+                            <li><FontAwesomeIcon className='pr-1 ' icon={faMessage} /> ishi13@gmail.com</li>
                         </ul>
                     </div>
                     <h1 className='pt-4 pl-5 text-xl font-semibold text-black'>Key skills</h1>
